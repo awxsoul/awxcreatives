cd68cb9c6f6a42c4d11b6854af94ef0972edd7d6
Gopi Krishna <awxsoul8@gmail.com>
2024-04-03 01:04:27 +0530

    Loop Transitions
    Smoothed loop transitions between start and end points. Defined scroll direction and specified different functions.

--- engine.js
+++ engine.js
@@ -18,22 +18,34 @@ function spin(){
     });   
 }
 
-function tr(){
-    document.getElementById("circle4").style.backgroundColor="black";
-}
 
 //Event Listeners
 window.addEventListener("resize", display);
-document.getElementById("projects").addEventListener("onclick",tr);
-//Inital funations to run
-display();
 
-console.log(window.scrollMaxY)
+
+  
+//Circular scroll Function
 window.addEventListener("scroll", ()=> {
+    // to move to scroll top of the page(start of the disc) when it hits the bottom
+    var maxscroll = document.body.scrollHeight/1.334
+    console.log(document.body.scrollHeight/1.334)
     console.log(window.scrollY)
-
-    if (window.scrollY>3000){
-        window.scrollTo(0,0)
+    if (this.oldScroll > this.scrollY){
+        // print "false" if direction is down and "true" if up
+        if (window.scrollY<1){
+            window.scrollTo(0,maxscroll)
+        }
+        console.log("True")
+        this.oldScroll = this.scrollY;
     }
-}
-)
+    else {
+        if (window.scrollY>maxscroll){
+            window.scrollTo(0,0)
+        }
+        console.log("False")
+        this.oldScroll = this.scrollY;
+    }  
+})
+
+//Inital functions to run
+display();
