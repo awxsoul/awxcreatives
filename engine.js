9d15995e90e84cc53dc103d3ff1d339960c5f346
Gopi Krishna <awxsoul8@gmail.com>
2024-05-30 13:52:44 +0530

    Optimizing Code
    - Commented all needed descriptions, references are mentioned
    - Padding was the issue for unplanned sizing difference of sections, when the resolution of the window is changed

--- engine.js
+++ engine.js
@@ -1,14 +1,15 @@
 
-const centrelogo = document.getElementById("centre_logo")
+//Variables ---------------X
 
 //Functions ---------------X
-function display(){
-    var digagonal_radius=((window.innerHeight)**2 + (window.innerWidth)**2)**(1/2)
-    document.getElementById("circle").style.height = digagonal_radius+"px";
-    document.getElementById("circle").style.width = digagonal_radius+"px";
+
+function display(){ //to make responsive window, using diagonal of the circle 
+    var diagonal_radius=((window.innerHeight)**2 + (window.innerWidth)**2)**(1/2)
+    document.getElementById("circle").style.height = diagonal_radius+"px";
+    document.getElementById("circle").style.width = diagonal_radius+"px";
 }
 
-function roatation_of_sections(){ //and arrrangement of sections
+function roatation_of_sections(){ //rotation and arrrangement of sections
     for (let i=1; i<17; i++){
         var block = document.getElementById("section"+i)
         angle=(360/16*i)
@@ -22,48 +23,31 @@ function roatation_of_sections(){ //and arrrangement of sections
     }
 }
 
-function poprotate(){
-    centrelogo.style.height=44+"%"
-}
-
-function project_details(){
-    for (let i=1; i<17; i++){
-        
-    }
-    
-}
-
 //Event Listeners ---------------X
 window.addEventListener("resize", display);
-centrelogo.addEventListener("mouseenter", poprotate);
 
-    //Circular Bi-directional Infinite Scroll Function
+//Circular Bi-directional Infinite Scroll Function
 window.addEventListener("scroll", ()=> {
     // to move to scroll top of the page(start of the disc) when it hits the bottom
     this.maxScroll = document.body.scrollHeight/1.334
-    //maxScroll and oldScroll are variable for this function
+        //maxScroll and oldScroll are variable for this function
         //maxScroll hold the max near value to end
         //oldScroll holds previous value
-    // console.log(document.body.scrollHeight/1.334)
-    // console.log(window.scrollY)
     if (this.oldScroll > this.scrollY){
         // print "false" if direction is down and "true" if up
         if (window.scrollY<1){
             window.scrollTo(0,this.maxScroll)
         }
-        // console.log("True")
         this.oldScroll = this.scrollY;
     }
     else {
         if (window.scrollY>this.maxScroll){
             window.scrollTo(0,0)
         }
-        // console.log("False")
         this.oldScroll = this.scrollY;
     }  
 })
 
-//Inital functions to run 
+//Inital functions to run ---------------X
 display();
 roatation_of_sections();
-centrewheel();
